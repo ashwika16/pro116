900b8c600a3354ab7245e74f0e6300d84583aede
ashwika16 <78404875+ashwika16@users.noreply.github.com>
2022-01-12 14:46:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0
+noseY=0
+
+function preload(){
+mustache=loadImage('https://i.postimg.cc/FspyW5Q9/Mustache.png')
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on ('pose',gotposes);
+}
+
+function modelLoaded()
+{
+
+    console.log('PoseNett is Initialized')
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+fill(255,0,0)
+stroke(255,0,0)
+image(mustache,noseX,noseY,30,30)
+
+}
+
+function take_snapshot(){
+    save('my.png');
+}
+
+function gotposes(results){
+if (results.lenght > 0)
+{
+    console.log(results);
+    noseX= results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x ="+ noseX);
+    console.log("nose y ="+ noseY);
+}
+}
